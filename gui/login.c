976a8aa6c703e0b439d12c003516ae4db31befb5
Andrew Berezovskyi <aa20062019aa@gmail.com>
2026-09-23 16:59:31 +0300

    feat: redesign the B-nix login experience

--- gui/login.c
+++ gui/login.c
@@ -61,9 +61,12 @@ void login_handle_keypress(char c) {
 }
 
 void login_process_mouse(int mx, int my, bool left_now, bool j_c) {
-    int cx = (int)d_screen_w / 2; int cy = (int)d_screen_h / 2;
-    int login_w = 400, login_h = 218; int login_y = cy - login_h / 2;
-    int btn_w = 150, btn_h = 34, btn_y = login_y + 166; int cancel_x = cx - 160, login_btn_x = cx + 10;
+    int cx = (int)d_screen_w / 2;
+    int cy = (int)d_screen_h / 2;
+    int login_w = 440, login_h = 360;
+    int login_y = cy - login_h / 2;
+    int btn_w = 155, btn_h = 38, btn_y = login_y + 298;
+    int cancel_x = cx - 165, login_btn_x = cx + 10;
 
     login_hover_cancel = point_in_rect(mx, my, cancel_x, btn_y, btn_w, btn_h);
     login_hover_login = point_in_rect(mx, my, login_btn_x, btn_y, btn_w, btn_h);
@@ -72,12 +75,14 @@ void login_process_mouse(int mx, int my, bool left_now, bool j_c) {
 
     if (j_c) {
         if (login_hover_cancel) {
-            username_len = 0; username_buffer[0] = '\0'; password_len = 0; password_buffer[0] = '\0';
+            username_len = 0; username_buffer[0] = '\0';
+            password_len = 0; password_buffer[0] = '\0';
             login_failed = false; active_field = 0;
-        } else if (login_hover_login) { try_login(); } 
-        else {
-            int field_w = 320, field_h = 44, field_x = cx - field_w / 2;
-            int user_y = login_y + 34; int pass_y = login_y + 86;
+        } else if (login_hover_login) {
+            try_login();
+        } else {
+            int field_w = 340, field_h = 46, field_x = cx - field_w / 2;
+            int user_y = login_y + 142, pass_y = login_y + 218;
             if (point_in_rect(mx, my, field_x, user_y, field_w, field_h)) active_field = 0;
             else if (point_in_rect(mx, my, field_x, pass_y, field_w, field_h)) active_field = 1;
         }
@@ -86,56 +91,87 @@ void login_process_mouse(int mx, int my, bool left_now, bool j_c) {
 
 void login_draw(uint32_t width, uint32_t height) {
     draw_cached_background();
-    const int top_h = 38; const float top_font = 22.0f; const int top_baseline_y = 28;
-    draw_filled_rect(0, 0, width, top_h, 0x1A1A1A);
+
+    draw_filled_rect(0, 0, width, 34, 0x121926);
+    draw_filled_rect(0, 33, width, 1, 0x34435A);
     if (main_font_data) {
-        draw_ttf_string(12, top_baseline_y, main_font_data, "B-nix", top_font, 0xBFD3E6);
+        draw_filled_circle(17, 17, 9, 0x6EA8FF);
+        draw_ttf_string(13, 22, main_font_data, "B", 13.0f, 0xFFFFFF);
+        draw_ttf_string(34, 23, main_font_data, "B-nix", 15.0f, 0xF5F8FC);
+        const char* secure = "Secure local session";
+        int sw = measure_ttf_text_width(main_font_data, secure, 13.0f);
+        draw_ttf_string((int)width - sw - 18, 22, main_font_data, secure, 13.0f, 0xAEBBCD);
     }
-    int cx = width / 2; int cy = height / 2;
-    int win_w = 400; int win_h = 218; int wx = cx - win_w / 2; int wy = cy - win_h / 2;
-    draw_rounded_rect(wx, wy, win_w, win_h, 16, 0x111111);
-    draw_cached_icon_centered(cx, wy + 40);
 
-    int field_w = 320; int field_h = 44; int field_x = cx - field_w / 2;
-    int text_baseline_offset = 30;
-    int username_text_px = measure_ttf_text_width(main_font_data, username_buffer, 18.0f);
-    int dot_pitch = measure_ttf_text_width(main_font_data, "*", 18.0f); if (dot_pitch < 10) dot_pitch = 10;
+    int cx = (int)width / 2, cy = (int)height / 2;
+    int win_w = 440, win_h = 360;
+    int wx = cx - win_w / 2, wy = cy - win_h / 2;
 
-    int user_y = wy + 34;
-    if (active_field == 0) draw_rounded_rect(field_x - 2, user_y - 2, field_w + 4, field_h + 4, 9, 0x1C315A);
-    draw_rounded_rect(field_x, user_y, field_w, field_h, 8, 0x0A0A0A);
-    if (active_field == 0) { draw_rounded_rect(field_x - 1, user_y - 1, field_w + 2, field_h + 2, 8, 0x2A4F90); draw_rounded_rect(field_x, user_y, field_w, field_h, 8, 0x0A0A0A); }
+    draw_rounded_rect(wx + 8, wy + 10, win_w, win_h, 22, 0x111827);
+    draw_rounded_rect(wx, wy, win_w, win_h, 22, 0xE8EDF5);
+    draw_rounded_rect(wx + 2, wy + 2, win_w - 4, win_h - 4, 20, 0xF7F9FC);
+
+    draw_filled_circle(cx, wy + 58, 34, 0xDCE9FF);
+    draw_filled_circle(cx, wy + 58, 29, 0x6EA8FF);
     if (main_font_data) {
-        if (username_len > 0) draw_ttf_string(field_x + 14, user_y + text_baseline_offset, main_font_data, username_buffer, 18.0, 0xFFFFFF);
-        else draw_ttf_string(field_x + 14, user_y + text_baseline_offset, main_font_data, "Username", 18.0, 0x666D78);
-    }
-    if (active_field == 0 && ((timer_ticks / 25) & 1u) == 0u) {
-        int caret_x = field_x + 14 + username_text_px + 1; draw_filled_rect(caret_x, user_y + 12, 2, 20, 0x88B8FF);
+        draw_ttf_string(cx - 10, wy + 70, main_font_data, "B", 27.0f, 0xFFFFFF);
+        const char* title = "Welcome to B-nix";
+        int tw = measure_ttf_text_width(main_font_data, title, 22.0f);
+        draw_ttf_string(cx - tw / 2, wy + 112, main_font_data, title, 22.0f, 0x1E293B);
+        const char* subtitle = "Sign in to continue to your desktop";
+        int subw = measure_ttf_text_width(main_font_data, subtitle, 13.0f);
+        draw_ttf_string(cx - subw / 2, wy + 132, main_font_data, subtitle, 13.0f, 0x718096);
     }
 
-    int pass_y = wy + 86;
-    if (active_field == 1) draw_rounded_rect(field_x - 2, pass_y - 2, field_w + 4, field_h + 4, 9, 0x1C315A);
-    draw_rounded_rect(field_x, pass_y, field_w, field_h, 8, 0x0A0A0A);
-    if (active_field == 1) { draw_rounded_rect(field_x - 1, pass_y - 1, field_w + 2, field_h + 2, 8, 0x2A4F90); draw_rounded_rect(field_x, pass_y, field_w, field_h, 8, 0x0A0A0A); }
+    int field_w = 340, field_h = 46, field_x = cx - field_w / 2;
+    int user_y = wy + 142, pass_y = wy + 218;
+
+    if (main_font_data) draw_ttf_string(field_x, user_y - 7, main_font_data, "Username", 12.0f, 0x526176);
+    draw_rounded_rect(field_x - 2, user_y - 2, field_w + 4, field_h + 4, 11,
+                      active_field == 0 ? 0x6EA8FF : 0xD4DBE5);
+    draw_rounded_rect(field_x, user_y, field_w, field_h, 9, 0xFFFFFF);
     if (main_font_data) {
-        if (password_len > 0) { for (int i = 0; i < password_len; i++) draw_filled_circle(field_x + 20 + (i * dot_pitch), pass_y + 22, 4, 0xFFFFFF); } 
-        else draw_ttf_string(field_x + 14, pass_y + text_baseline_offset, main_font_data, "Password", 18.0, 0x666D78);
+        const char* text = username_len > 0 ? username_buffer : "admin";
+        uint32_t color = username_len > 0 ? 0x1F2937 : 0xA1AAB8;
+        draw_ttf_string(field_x + 15, user_y + 30, main_font_data, text, 17.0f, color);
+        if (active_field == 0 && ((timer_ticks / 25) & 1u) == 0u) {
+            int px = username_len > 0 ? measure_ttf_text_width(main_font_data, username_buffer, 17.0f) : 0;
+            draw_filled_rect(field_x + 15 + px, user_y + 12, 2, 22, 0x4B8EF7);
+        }
     }
-    if (active_field == 1 && ((timer_ticks / 25) & 1u) == 0u) {
-        int caret_x = field_x + 20 + (password_len * dot_pitch) + 1; draw_filled_rect(caret_x, pass_y + 12, 2, 20, 0x88B8FF);
+
+    if (main_font_data) draw_ttf_string(field_x, pass_y - 7, main_font_data, "Password", 12.0f, 0x526176);
+    draw_rounded_rect(field_x - 2, pass_y - 2, field_w + 4, field_h + 4, 11,
+                      active_field == 1 ? 0x6EA8FF : 0xD4DBE5);
+    draw_rounded_rect(field_x, pass_y, field_w, field_h, 9, 0xFFFFFF);
+    int dot_pitch = 13;
+    if (main_font_data) {
+        if (password_len == 0) {
+            draw_ttf_string(field_x + 15, pass_y + 30, main_font_data, "Enter password", 17.0f, 0xA1AAB8);
+        } else {
+            for (int i = 0; i < password_len; i++)
+                draw_filled_circle(field_x + 20 + i * dot_pitch, pass_y + 23, 4, 0x334155);
+        }
+        if (active_field == 1 && ((timer_ticks / 25) & 1u) == 0u)
+            draw_filled_rect(field_x + 18 + password_len * dot_pitch, pass_y + 12, 2, 22, 0x4B8EF7);
     }
 
-    if (login_failed && main_font_data) draw_ttf_string(cx - 100, pass_y + 62, main_font_data, "Authentication failed!", 16.0, 0xFF3333);
+    if (login_failed && main_font_data)
+        draw_ttf_string(field_x, pass_y + 63, main_font_data,
+                        "Incorrect username or password", 13.0f, 0xD83A52);
 
-    int btn_w = 150; int btn_h = 34; int btn_y = wy + 166; int cancel_x = cx - 160;
-    uint32_t cancel_fill = login_pressed_cancel ? 0x1E1E1E : (login_hover_cancel ? 0x2E2E2E : 0x2A2A2A);
-    uint32_t cancel_border = login_pressed_cancel ? 0x222222 : (login_hover_cancel ? 0x4A4A4A : 0x333333);
-    draw_rounded_rect(cancel_x, btn_y, btn_w, btn_h, 6, cancel_fill); draw_rect_outline(cancel_x, btn_y, btn_w, btn_h, cancel_border);
-    if (main_font_data) { int tw = measure_ttf_text_width(main_font_data, "Cancel", 16.0f); draw_ttf_string(cancel_x + (btn_w - tw) / 2, btn_y + 23, main_font_data, "Cancel", 16.0, 0xD4D8DE); }
+    int btn_w = 155, btn_h = 38, btn_y = wy + 298, cancel_x = cx - 165, login_x = cx + 10;
+    uint32_t cancel_fill = login_pressed_cancel ? 0xD5DBE5 : (login_hover_cancel ? 0xE2E7EF : 0xEDF1F6);
+    draw_rounded_rect(cancel_x, btn_y, btn_w, btn_h, 10, cancel_fill);
+    draw_rect_outline(cancel_x, btn_y, btn_w, btn_h, 0xC8D0DC);
 
-    int login_x = cx + 10;
-    uint32_t login_fill = login_pressed_login ? 0x2E57AD : (login_hover_login ? 0x3C72DC : 0x3366CC);
-    uint32_t login_border = login_pressed_login ? 0x3A6FD8 : (login_hover_login ? 0x66A4FF : 0x4488FF);
-    draw_rounded_rect(login_x, btn_y, btn_w, btn_h, 6, login_fill); draw_rect_outline(login_x, btn_y, btn_w, btn_h, login_border);
-    if (main_font_data) { int tw = measure_ttf_text_width(main_font_data, "Log In", 16.0f); draw_ttf_string(login_x + (btn_w - tw) / 2, btn_y + 23, main_font_data, "Log In", 16.0, 0xFFFFFF); }
+    uint32_t login_fill = login_pressed_login ? 0x3978D8 : (login_hover_login ? 0x77AEFF : 0x5795F2);
+    draw_rounded_rect(login_x, btn_y, btn_w, btn_h, 10, login_fill);
+
+    if (main_font_data) {
+        int cw = measure_ttf_text_width(main_font_data, "Clear", 15.0f);
+        draw_ttf_string(cancel_x + (btn_w - cw) / 2, btn_y + 25, main_font_data, "Clear", 15.0f, 0x334155);
+        int lw = measure_ttf_text_width(main_font_data, "Continue", 15.0f);
+        draw_ttf_string(login_x + (btn_w - lw) / 2, btn_y + 25, main_font_data, "Continue", 15.0f, 0xFFFFFF);
+    }
 }
